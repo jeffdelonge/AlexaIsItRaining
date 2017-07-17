dc88168b26e62acd84923d088349ecd851754797
Jeff DeLonge <jeff.delonge@yahoo.com>
2017-07-17 12:29:14 -0400

    Third submission for publishing
    - Fix session management
    - Rephrase speech prompts

--- src/intent_handlers.py
+++ src/intent_handlers.py
@@ -3,26 +3,23 @@ import urllib.request
 
 # Intent handlers & speech utilities --------------------------------------------------------------------------------------------
 WELCOME_TEXT = "Hello! Give me a city to check for rain."
-HELP_TEXT = "You can say things like, New York. Is it raining in Sydney, Australia? You can also say exit, to quit."
+HELP_TEXT = "You can say things like, New York. Or, is it raining in Sydney Australia? When you're ready, give me a city to check for rain."
 GOODBYE_TEXT = "Goodbye."
-UNRECOGNIZED_CITY_TEXT = "Sorry, I didn't recognize that city. Please try again."
-HTTP_ERROR_TEXT = "Give me another city or say exit, to quit."
-STANDARD_REPROMPT = "Sorry, I didn't catch that. Please try again or say help, for assistance."
-IS_IT_RAINING_REPROMPT = "Give me another city or say exit, to quit."
+UNRECOGNIZED_CITY_TEXT = "Sorry, I didn't recognize that city. Please try again. Or say, help, for assistance."
+HTTP_ERROR_TEXT = "Oops, an error occured. Please try again. Or say, exit, to quit."
+STANDARD_REPROMPT = "Give me a city to check for rain. Or say, exit, to quit."
 
 def is_it_raining_in_location(intent):
     try:
         city = intent['slots']['City']['value'].replace(' ', '-')
         location = city
     except KeyError:
-        return build_response(UNRECOGNIZED_CITY_TEXT, reprompt_speech_text=IS_IT_RAINING_REPROMPT)
-
+        return build_response(UNRECOGNIZED_CITY_TEXT, reprompt_speech_text=STANDARD_REPROMPT)
     try:
         state = intent['slots']['State']['value'].replace(' ', '-')
         location += '-' + state
     except KeyError:
         pass
-
     try:
         country = intent['slots']['Country']['value'].replace(' ', '-')
         location += '-' + country
@@ -52,17 +49,17 @@ def is_it_raining_in_location(intent):
             result = 'Yes. It is raining in {}'.format(location_result)
         else:
             result = 'No. It is not raining in {}'.format(location_result)
+        return build_response(result, should_end_session=True)
     except urllib.error.HTTPError as e:
         if (e.code == 404):
             result = UNRECOGNIZED_CITY_TEXT
         else:
             result = HTTP_ERROR_TEXT
-
-    return build_response(result, reprompt_speech_text=IS_IT_RAINING_REPROMPT)
+        return build_response(result, reprompt_speech_text=STANDARD_REPROMPT)
 
 
 def get_help_response():
-    return build_response(HELP_TEXT)
+    return build_response(HELP_TEXT, reprompt_speech_text=STANDARD_REPROMPT)
 
 
 def get_goodbye_response():
--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,10 +1,9 @@
+import constants
 from request_handlers import *
-
-IS_IT_RAINING_APPLICATION_ID = "amzn1.ask.skill.9d78870e-6c3b-4ced-9894-412f5d84f25e"
 
 def lambda_handler(event, context):
     # Ensure that the request was intended for our service
-    if (event['session']['application']['applicationId'] != IS_IT_RAINING_APPLICATION_ID):
+    if (event['session']['application']['applicationId'] != constants.IS_IT_RAINING_APPLICATION_ID):
         raise ValueError("Invalid applicationId")
 
     # Get the request type and call the corresponding request handler
